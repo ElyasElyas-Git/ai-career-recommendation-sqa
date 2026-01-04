4993bc2992d1f95e0887d93bf5bf2fa18d1aebaf
ElyasElyas-Git <23F24127@mec.edu.om>
2026-01-04 19:06:19 +0400

    Fixes #1: Added input validation for module count and marks

--- career_recommendation_algorithm.py
+++ career_recommendation_algorithm.py
@@ -1,9 +1,21 @@
 START
 Input number_of_modules
+
+WHILE number_of_modules ≤ 0
+ Display "Error: Number of modules must be greater than 0"
+Input number_of_modules
+END WHILE
+
 Set total = 0
 
 FOR i = 1 TO number_of_modules
  Input module_mark
+
+WHILE module_mark < 0 OR module_mark > 100
+ Display "Error: Mark must be between 0 and 100"
+ Input module_mark
+END WHILE
+
  total = total + module_mark
 END FOR
 
